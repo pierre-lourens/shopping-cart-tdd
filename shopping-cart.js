db41c55bf2b85ba19624131fa5be960f1819cdc7
Pierre Lourens <plourens3@gmail.com>
2020-06-10 11:21:23 -0400

    Got second test to pass with qty and items

--- shopping-cart.js
+++ shopping-cart.js
@@ -1,5 +1,12 @@
 module.exports = class ShoppingCart {
+  constructor(shoppingList = []) {
+    this.shoppingList = shoppingList;
+  }
+
   get subtotal() {
-    return 0;
+    return this.shoppingList.reduce((acc, currentItem) => {
+      acc += currentItem.quantity * currentItem.price;
+      return acc;
+    }, 0);
   }
 };
